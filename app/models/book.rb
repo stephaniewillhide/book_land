52a15abe98839c5080ad88ae2b14079a1a0d0583
Stephanie Willhide <swillhide@interexchange.org>
2019-05-12 21:55:06 -0400

    validate presence of both isbn and name

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ApplicationRecord
   validates :isbn,
+            presence: true,
             uniqueness: true,
             length: { is: 10 || 13 }
+
+  validates :name, presence: true
 end
