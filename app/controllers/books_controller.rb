7de699a884415163e97fb0f43168005844435b9b
Stephanie Willhide <swillhide@interexchange.org>
2019-05-12 23:14:02 -0400

    update layout, form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,22 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.ordered
+  end
+
   def new
+    @book = Book.new
   end
 
   def create
-    def create
-      render plain: params[:book].inspect
+    @book = Book.new
+    if @book.save
+      redirect_to(books_path, notice: "#{ @book.name } successfully created.")
+    else
+      render :new
     end
   end
 
-  def index
-    @books = Book.all
+  def edit
   end
 end
