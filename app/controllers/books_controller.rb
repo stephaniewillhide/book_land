491e1961f5cc0cd02af6168b4ddb2a23a88e49cd
Stephanie Willhide <swillhide@interexchange.org>
2019-05-12 23:44:15 -0400

    fix email error wip

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :find_book, only: [:edit, :update]
 
   def index
     @books = Book.ordered
@@ -18,5 +19,17 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @book.update(object_params)
+      redirect_to(bookss_path, notice: "#{ @book.name } successfully updated.")
+    else
+      render :edit
+    end
+  end
+
+  private def object_params
+    params.require(:book).permit(:name, :isbn)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,13 @@
 class Book < ApplicationRecord
+
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
   validates :isbn,
             presence: true,
             uniqueness: true,
             length: { is: 10 || 13 }
 
   validates :name, presence: true
+
+  scope :ordered, -> { order(name: :asc) }
 end
