34efa250b41010357939844aca42e2aa8893b373
Stephanie Willhide <swillhide@interexchange.org>
2019-05-12 22:54:44 -0400

    define create in applications controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,13 @@
 class BooksController < ApplicationController
+  def new
+  end
+
+  def create
+    @book = Book.new
+    if @book.save
+      redirect_to(books_path, notice: "#{ @book.name } successfully created.")
+    else
+      render :new
+    end
+  end
 end
