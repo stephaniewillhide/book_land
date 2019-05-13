e986efe7a46c5f7a714ca18676bff799b91f95b0
Stephanie Willhide <swillhide@interexchange.org>
2019-05-12 22:32:28 -0400

    add books controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,7 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_063610) do
+ActiveRecord::Schema.define(version: 2019_05_13_014444) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "isbn"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
