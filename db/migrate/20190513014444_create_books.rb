3fe784d8b9683d562a068f9be8c2a267e508f94b
Stephanie Willhide <swillhide@interexchange.org>
2019-07-06 18:33:42 -0400

    add null: false to migration

--- db/migrate/20190513014444_create_books.rb
+++ db/migrate/20190513014444_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :name
-      t.string :isbn
+      t.string :name, null: false
+      t.string :isbn, null: false
 
       t.timestamps
     end
