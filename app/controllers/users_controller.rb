d727c4f513e8d9dc38c55ae852a676dbc7a064cc
Stephanie Willhide <swillhide@interexchange.org>
2019-08-18 16:44:04 -0600

    Admins can search for users by name or email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApplicationController
   before_action :find_user, only: [:edit, :update]
 
   def index
-    @users = User.ordered.page params[:page]
+    @search_term = params.dig(:search, :term)
+    search_term_with_wildcards = "%#{@search_term}%"
+    @users = User.ordered.page(params[:page])
+      .where("name LIKE ? OR email LIKE ?", search_term_with_wildcards, search_term_with_wildcards)
   end
 
   def new
