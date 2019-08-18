7f647fc4b7c64be20fac5b6b85e4454186344835
Stephanie Willhide <swillhide@interexchange.org>
2019-08-18 16:39:50 -0600

    add 4 books to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@ end
 unless User.exists?(email: "swillhide@interexchange.org")
   User.create!(name: "Stephanie Willhide", email: "swillhide@interexchange.org", password: "password")
 end
+
+unless Book.exists?(isbn: "0393312836")
+  Book.create!(name: "A Clockwork Orange", isbn: "0393312836")
+end
+
+unless Book.exists?(isbn: "0679734503")
+  Book.create!(name: "Crime and Punishment", isbn: "0679734503")
+end
+
+unless Book.exists?(isbn: "0679734503")
+  Book.create!(name: "Crime and Punishment", isbn: "0679734503")
+end
+
+unless Book.exists?(isbn: "0321410971")
+  Book.create!(name: "Transcending CSS", isbn: "0321410971")
+end
+
+unless Book.exists?(isbn: "0399226907")
+  Book.create!(name: "The Very Hungry Caterpillar", isbn: "0399226907")
+end
