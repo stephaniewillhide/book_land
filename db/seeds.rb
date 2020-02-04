d410881fe8bd2b5c0ce51dbac3fffec5fa21295d
Stephanie Willhide <swillhide@interexchange.org>
2020-02-03 19:58:56 -0700

    Add cover imgs to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@ unless User.exists?(email: "swillhide@interexchange.org")
 end
 
 [
-  ["A Clockwork Orange", "0393312836"],
-  ["Crime and Punishment", "0679734503"],
-  ["Transcending CSS", "0321410971"],
-  ["The Very Hungry Caterpillar", "0399226907"]
+  ["A Clockwork Orange", "0393312836", "/db/sample_data/a-clockwork-orange.jpg"],
+  ["Crime and Punishment", "0679734503", "/db/sample_data/crime-and-punishment.jpg"],
+  ["Transcending CSS", "0321410971", "/db/sample_data/transcending-css.jpg"],
+  ["The Very Hungry Caterpillar", "0399226907", "/db/sample_data/the-very-hungry-caterpillar.jpg"]
 ].each do |name, isbn|
-  Book.find_or_create_by(name: name, isbn: isbn)
+  Book.find_or_create_by(name: name, isbn: isbn, cover: cover)
 end
