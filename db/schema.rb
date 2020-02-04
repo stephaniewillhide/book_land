305f6e36e6611dc1c61afecb2fe6785859527dd3
Stephanie Willhide <swillhide@interexchange.org>
2020-02-03 20:12:28 -0700

    Add cover to schema

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_08_19_005449) do
   create_table "books", force: :cascade do |t|
     t.string "name", null: false
     t.string "isbn", null: false
+    t.string "cover"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
