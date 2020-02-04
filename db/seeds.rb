f8dc34d76a88905f8dbce662df8b41e12a1bc36d
Stephanie Willhide <swillhide@interexchange.org>
2020-02-03 20:12:58 -0700

    Loop through cover

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@ end
   ["Crime and Punishment", "0679734503", "/db/sample_data/crime-and-punishment.jpg"],
   ["Transcending CSS", "0321410971", "/db/sample_data/transcending-css.jpg"],
   ["The Very Hungry Caterpillar", "0399226907", "/db/sample_data/the-very-hungry-caterpillar.jpg"]
-].each do |name, isbn|
+].each do |name, isbn, cover|
   Book.find_or_create_by(name: name, isbn: isbn, cover: cover)
 end
