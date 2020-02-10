dd7d6b6f177277d876b7d21e6b92984f7e24359e
Stephanie Willhide <swillhide@interexchange.org>
2020-02-09 19:21:19 -0700

    Add Featured Column

--- db/migrate/20190513014444_create_books.rb
+++ db/migrate/20190513014444_create_books.rb
@@ -3,6 +3,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.string :name, null: false
       t.string :isbn, null: false
+      t.boolean :featured
 
       t.timestamps
     end
