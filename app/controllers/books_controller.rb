a5c5a8b0bc7aaf26c5451c6f7e17957b2db0fda3
Stephanie Willhide <swillhide@interexchange.org>
2020-08-20 20:03:05 -0600

    Date fix wip

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.csv { send_data @books.to_csv }
+      format.csv { send_data @books.to_csv, filename: "#{Date.today}_books.csv" }
     end
   end
 
